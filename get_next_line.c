e4dd4af69d9ee6333fa4d3dbe9c46835e39d4320
soong-e <sksuwjd@naver.com>
2022-07-18 21:53:16 +0900

    leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: yujelee <yujelee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:56:39 by yujelee           #+#    #+#             */
-/*   Updated: 2022/07/16 18:41:03 by yujelee          ###   ########.fr       */
+/*   Updated: 2022/07/18 21:45:11 by yujelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-#define BUFFER_SIZE 42
+#include <fcntl.h>
+//#define BUFFER_SIZE 42
 
 int	find_nl(char *str, int len)
 {
@@ -21,14 +22,14 @@ int	find_nl(char *str, int len)
 	idx = 0;
 	while (len--)
 	{
-		if (str[idx] == '\n')
+		if (str[idx] == '\n' || str[idx] == 0)
 			return (idx);
 		idx++;
 	}
-	return (-1);
+	return (idx);
 }
 
-char	*strjoin(char *s1, char *s2, unsigned int s1size, unsigned int s2size)
+char	*strjoin(char *s1, char *s2, int s1size, int s2size)
 {
 	int		amount;
 	char	*ret;
@@ -43,20 +44,20 @@ char	*strjoin(char *s1, char *s2, unsigned int s1size, unsigned int s2size)
 		return (NULL);
 	idx1 = 0;
 	idx2 = 0;
-	while (s1[idx1])
+	while (idx1 < s1size - 1)
 		ret[idx2++] = s1[idx1++];
 	idx1 = 0;
-	while (s2[idx1])
+	while (idx1 < s2size)
 		ret[idx2++] = s2[idx1++];
-	ret[idx2] = 0;
+	free(s1);
 	return (ret);
 }
 
-char	*split(char *temp, char *str, unsigned int strsize)
+char	*split(char *temp, char *str, int strsize)
 {
 	char	*ret;
-	int				loc;
-	int				idx;
+	int		loc;
+	int		idx;
 
 	free(temp);
 	loc = find_nl(str, strsize);
@@ -70,62 +71,81 @@ char	*split(char *temp, char *str, unsigned int strsize)
 	return (ret);
 }
 
-char	*makenewstr(char *str, unsigned int strsize)
+char	*makenewstr(char *str, int *strsize)
 {
 	char	*ret;
-	unsigned int	loc;
-	int	idx;
+	int		loc;
+	int		temp;
+	int		idx;
 
-	loc = find_nl(str, strsize);
-	ret = (char *)malloc((strsize - loc) * sizeof(char));
+	loc = find_nl(str, *strsize);
+	temp = *strsize - loc;
+	ret = (char *)malloc((*strsize - loc) * sizeof(char));
 	if (!ret)
 		return (NULL);
 	idx = -1;
-	while (loc < strsize)
+	while (loc < *strsize)
 		ret[++idx] = str[++loc];
+	*strsize = temp;
+	free(str);
 	return (ret);
-}
-
-void freeall()
-{
-
 }
 
 char	*get_next_line(int fd)
 {
-	static char			*ret;
-	char				*temp;
-	static unsigned int	retsize;
-	unsigned int		tempsize;
+	static char	*ret;
+	char		*temp;
+	static int	retsize;
+	int			tempsize;
 
 	temp = (char *)malloc(BUFFER_SIZE * sizeof(char));
 	if (!temp)
 		return (NULL);
-	tempsize = read(fd, temp, BUFFER_SIZE);
-	if (tempsize == 0)
-			return (NULL);
-	retsize = 0;
-	while (tempsize)
+  	tempsize = read(fd, temp, BUFFER_SIZE);
+	if (tempsize == 0 && retsize == 0)
+	{
+		free(temp);
+		return (NULL);
+	}
+	while (tempsize || retsize)
 	{
 		ret = strjoin(ret, temp, retsize, tempsize);
 		retsize += tempsize;
-		if (find_nl(temp, tempsize) < BUFFER_SIZE)
-			break;
+		if (find_nl(ret, retsize) < retsize)
+			break ;
 		tempsize = read(fd, temp, BUFFER_SIZE);
 	}
 	temp = split(temp, ret, retsize);
 	if (!temp)
 		return (NULL);
-	ret = makenewstr(ret, retsize);
+	ret = makenewstr(ret, &retsize);
 	if (!ret)
 		return (NULL);
-	retsize -= find_nl(ret, retsize);
 	return (temp);
 }
 
+/*
 int main()
 {
 	int fd = open("test.txt", O_RDONLY);
 
-	get_next_line(fd);
+	printf("result1 -> %s \n", get_next_line(fd));
+	printf("result2 -> %s \n", get_next_line(fd));
+	printf("result3 -> %s \n", get_next_line(fd));
+	printf("result4 -> %s \n", get_next_line(fd));
+	printf("result5 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	printf("result6 -> %s \n", get_next_line(fd));
+	
 }
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,21 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yujelee <yujelee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/18 21:20:40 by yujelee           #+#    #+#             */
+/*   Updated: 2022/07/18 21:21:53 by yujelee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
+
+#endif
