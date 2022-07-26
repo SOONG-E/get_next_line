6c12b9a5b6e1620543fdc48aa357a42194e4532e
soong-e <sksuwjd@naver.com>
2022-07-26 20:54:55 +0900

    ha..

--- get_next_line.c
+++ get_next_line.c
@@ -3,156 +3,175 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yujelee <yujelee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/16 15:56:39 by yujelee           #+#    #+#             */
-/*   Updated: 2022/07/18 21:54:33 by yujelee          ###   ########.fr       */
+/*   Created: 2022/07/26 17:08:33 by yujelee           #+#    #+#             */
+/*   Updated: 2022/07/26 20:54:37 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
-//#define BUFFER_SIZE 42
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h> //ㅈㅔ바ㄹ 제제출출하하기  전전에  삭삭제제하하기  제제발  제제바바제제바바
+#include <stdio.h> //ㅈㅔ바ㄹ 제제출출하하기  전전에  삭삭제제하하기  제제발  제제바바제제바바
+#define BUFFER_SIZE 2
 
-int	find_nl(char *str, int len)
+int	ft_strlen(char *str, int flag)
 {
-	int	idx;
+	int	len;
 
-	idx = 0;
-	while (len--)
+	len = 0;
+	if (!str)
+		return (0);
+	if (flag)
 	{
-		if (str[idx] == '\n'|| str[idx] == 0)
-			return (idx);
-		idx++;
+		while (str[len])
+			len++;
+		return (len);
 	}
-	return (idx);
+	while (str[len] && str[len] != '\n')
+		len++;
+	return (len);
 }
 
-char	*strjoin(char *s1, char *s2, int s1size, int s2size)
+char	*strjoin(char *str1, char *str2)
 {
+	int		retidx;
+	int		stridx;
 	int		amount;
 	char	*ret;
-	int		idx1;
-	int		idx2;
 
-	if (!s2)
-		return (NULL);
-	amount = s1size + s2size;
+	amount = ft_strlen(str1, 1) + ft_strlen(str2, 1);
 	ret = (char *)malloc((amount + 1) * sizeof(char));
 	if (!ret)
 		return (NULL);
-	idx1 = 0;
-	idx2 = 0;
-	while (idx1 < s1size)
-		ret[idx2++] = s1[idx1++];
-	idx1 = 0;
-	while (idx1 < s2size)
-		ret[idx2++] = s2[idx1++];
-	free(s1);
+	retidx = 0;
+	if (str1)
+	{
+		stridx = 0;
+		while (str1[stridx])
+			ret[retidx++] = str1[stridx++];
+		free(str1);
+	}
+	stridx = 0;
+	while (str2[stridx])
+		ret[retidx++] = str2[stridx++];
+	ret[retidx] = 0;
 	return (ret);
 }
 
-char	*split(char *temp, char *str, int strsize)
+char	*read_temp(int fd, char *ret, char *temp)
 {
-	char	*ret;
-	int		loc;
-	int		idx;
+	int	idx;
+	int	tempidx;
+
+	printf("read_temp %s\n", ret);
+	idx = read(fd, temp, BUFFER_SIZE);
+	if (!idx)
+		return (ret);
+	while (idx)
+	{
+		temp[idx] = 0;
+		tempidx = 0;
+		printf("# -> %s\n", temp);
+		while (temp[tempidx] && temp[tempidx] != '\n')
+			tempidx++; 
+		ret = strjoin(ret, temp);
+		//printf("@@%s\n", ret);
+		if (!ret)
+			return (NULL);
+		//printf("SIZE!!! %d %d\n", tempidx, idx);
+		if (tempidx < idx)
+			break;
+		idx = read(fd, temp, BUFFER_SIZE);
+	}
+	//printf("****%s****\n", ret);
+	return (ret);
+}
+
+char	*temp_split(char *temp, char *ret)
+{
+	int	idx;
 
 	free(temp);
-	loc = find_nl(str, strsize);
-	ret = (char *)malloc((loc + 1) * sizeof(char));
-	if (!ret)
+	temp = (char *)malloc((ft_strlen(ret, 0) + 1) * sizeof(char));
+	if (!temp)
 		return (NULL);
 	idx = -1;
-	while (++idx < loc)
-		ret[idx] = str[idx];
-	ret[++idx] = 0;
-	return (ret);
+	while (ret[++idx] != '\n')
+		temp[idx] = ret[idx];
+	if (ret[idx] == '\n')
+		temp[idx] = '\n';
+	temp[++idx] = 0;
+	return (temp);
 }
 
-char	*makenewstr(char *str, int *strsize)
+char	*remain_ret(char *ret)
 {
-	char	*ret;
-	int		loc;
-	int		temp;
+	char	*newret;
 	int		idx;
+	int		newidx;
 
-	loc = find_nl(str, *strsize);
-	temp = *strsize - loc - 1;
-	ret = (char *)malloc((*strsize - loc) * sizeof(char));
-	if (!ret)
+	printf("@@@ %d %d\n", ft_strlen(ret, 0), ft_strlen(ret, 1));
+	idx = ft_strlen(ret, 0);
+	newret = (char *)malloc((ft_strlen(ret, 1) - idx + 1) * sizeof(char));
+	if (!newret)
 		return (NULL);
-	idx = -1;
-	while (loc < *strsize)
-		ret[++idx] = str[++loc];
-	*strsize = temp;
-	free(str);
-	return (ret);
+	newidx = 0;
+	while (ret[++idx])
+		newret[newidx++] = ret[idx];
+	newret[newidx] = 0;
+	free(ret);
+	printf("newret %s\n", newret);
+	return (newret);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*ret;
 	char		*temp;
-	static int	retsize;
-	int			tempsize;
 
-	if (fd < 0)
+	printf("at the first time %s-\n", ret);
+	temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!temp || fd < 0)
 		return (NULL);
-	temp = (char *)malloc(BUFFER_SIZE * sizeof(char));
-	if (!temp)
-		return (NULL);
-  	tempsize = read(fd, temp, BUFFER_SIZE);
-	if (tempsize == 0 && retsize <= 0)
-	{
-		free(temp);
-		return (NULL);
-	}
-	while (tempsize || retsize)
-	{
-		ret = strjoin(ret, temp, retsize, tempsize);
-		if (!ret)
-			return (NULL);
-		retsize += tempsize;
-		if (find_nl(ret, retsize) < retsize || !tempsize)
-			break ;
-		tempsize = read(fd, temp, BUFFER_SIZE);
-	}
-	temp = split(temp, ret, retsize);
-	if (!temp)
-		return (NULL);
-	ret = makenewstr(ret, &retsize);
+	ret = read_temp(fd, ret, temp);
 	if (!ret)
 		return (NULL);
-	//if (retsize <= 0&& !tempsize && ret)
-	//	free(ret);
-	//printf("#END %d\n#%s\n", retsize, ret);
+	temp = temp_split(temp, ret);
+	if (!temp)
+		return (NULL);
+	ret = remain_ret(ret);
+	if (!ret)
+		return (NULL);
+	printf("ret changed %s-\n", ret);
+	if (!ft_strlen(ret, 1))
+		free(ret);
 	return (temp);
 }
 
-/*
+//ㅈㅔ바ㄹ 제제출출하하기  전전에  삭삭제제하하기  제제발  제제바바제제바바
 int main()
 {
 	int fd = open("test.txt", O_RDONLY);
 
 	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-	printf("result1 -> %s \n", get_next_line(fd));
+	printf("result1 -> %s", get_next_line(fd));
 	
-	printf("result2 -> %s \n", get_next_line(fd));
+	printf("result2 -> %s", get_next_line(fd));
 	
-	printf("result3 -> %s \n", get_next_line(fd));
-	printf("result4 -> %s \n", get_next_line(fd));
-	printf("result5 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	printf("result6 -> %s \n", get_next_line(fd));
-	//printf("result6 -> %s \n", get_next_line(fd));
-	
-}*/
+	printf("result3 -> %s", get_next_line(fd));
+	printf("result4 -> %s", get_next_line(fd));
+	printf("result5 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	printf("result6 -> %s", get_next_line(fd));
+	//printf("result6 -> %s", get_next_line(fd));
+}
